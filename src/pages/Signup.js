18ee2f773568b887a629092204e604e365c0a9ee
Prahlad Inala <prahlad2018@gmail.com>
2023-10-31 15:53:47 +0530

    Signup page done

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -18,6 +18,23 @@ export default function Signup() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
@@ -69,7 +86,7 @@ export default function Signup() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Already a member?{" "}
             <Link
-              to="/signup"
+              to="/login"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Login
