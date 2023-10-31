2b6726fab3efab89cf7b33f42300069ffebde7a5
Prahlad Inala <prahlad2018@gmail.com>
2023-10-31 16:16:37 +0530

    NewsLetter Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import NewsLetter from "../components/NewsLetter";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <NewsLetter />
       <Footer />
     </>
   );
