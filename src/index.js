c62ffecff6d5a0ddf8f9cf2efbe99f5540d6ab5a
Prahlad Inala <prahlad2018@gmail.com>
2023-10-31 14:53:01 +0530

    NavBar and Route Setup Done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<h1>Test - But not a good approch</h1>} />
+        <Route path="/myapps" element={<Navigate to="/learn" />} />
+        <Route path="/apps" element={<Navigate replace to="/learn" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
